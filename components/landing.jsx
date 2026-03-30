e67db735e86860ba97ee35bc24c73f2dbd92ea8a
GlycerolVeinz <matvej.safrankov@gmail.com>
2026-03-30 14:58:26 +0200

    fixed legacy next Image

--- components/landing.jsx
+++ components/landing.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useRef } from 'react';
-import Image from "next/legacy/image";
+import Image from "next/image";
 import { ENV } from '@/components/config';
 import { useOrientation } from '@/lib/hooks/useOrientation';
 import textStyles from '@/components/styles/globals/text.module.css';
@@ -15,18 +15,20 @@ export default function Landing() {
     return (
         <section className={pageStyles.landingSection} id="landing" ref={landingRef}>
             <div className={pageStyles.backgroundImageWrapper}>
-                <Image className={pageStyles.backgroundImage}
+                <Image
+                    className={pageStyles.backgroundImage}
                     src={isPortrait ? ENV.BACKGROUND_IMAGE_PORTRAIT : ENV.BACKGROUND_IMAGE_LANDSCAPE}
-                    layout="fill"
-                    objectFit="cover"
                     alt="Band Background"
-                />
+                    fill
+                    sizes="100vw"
+                    style={{
+                        objectFit: "cover"
+                    }} />
             </div>
-
             <div className={pageStyles.landingContent}>
                 <h1 className={`${textStyles.bandNameText} band-name-font`}>Temporary</h1>
                 <h1 className={`${textStyles.bandNameText} band-name-font`}>Friends</h1>
             </div>
         </section>
-    )
+    );
 }
